662ecb240bc013eeaea5327268c363f3ce36bc0c
Ammar Bandukwala <ammar@ammar.io>
2022-01-23 17:43:21 -0600

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,19 @@ function MyApp({Component, pageProps}: AppProps) {
         <>
             <Head>
                 <title>Half-Life Calculator – grams.io</title>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-RZ3SWVWV2J"/>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-RZ3SWVWV2J');
+                    `
+                }}>
+                </script>
                 <meta name="viewport" content= "width=device-width, user-scalable=no" />
-                <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
+                <link rel="stylesheet" href="https://rsms.me/inter/inter.css"/>
             </Head>
             <Component {...pageProps} />
         </>
