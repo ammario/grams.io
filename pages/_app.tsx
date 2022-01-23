f399cab1e01fac2ba948ed54c4f15ab6acfac5f1
Ammar Bandukwala <ammar@ammar.io>
2022-01-23 15:35:34 -0600

    Merge styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,8 @@ function MyApp({Component, pageProps}: AppProps) {
     return (
         <>
             <Head>
-                <title>Half-life calculator – grams.io</title>
+                <title>Half-Life Calculator – grams.io</title>
+                <meta name="viewport" content= "width=device-width, user-scalable=no" />
                 <link rel="stylesheet" href="https://rsms.me/inter/inter.css"></link>
             </Head>
             <Component {...pageProps} />
