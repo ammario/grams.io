cf4fb521b83bd51722bb64db8156430bd485020d
David Alsh <alshdavid@gmail.com>
2025-09-19 17:04:22 +1000

    Externalized drug list and moved graph to top of the page

--- next.config.js
+++ next.config.js
@@ -8,6 +8,19 @@ const nextConfig = {
     fiber: false,
     includePaths: [path.join(__dirname, "styles")],
   },
+  webpack: (config, _) => {
+    config.module.rules.push({
+      test: /\.yaml$/,
+      use: {
+        loader: 'yaml-loader',
+        options: {
+          // loader options
+        }
+      }
+    });
+
+    return config
+  }
 };
 
 module.exports = nextConfig;
